0f94ab80b1dbb2e4a432d58e70ffe990630148d2
AllenXP20 <73925320+AllenXP20@users.noreply.github.com>
2021-01-07 01:13:42 +0630

    user edit and change password

--- routes/web.php
+++ routes/web.php
@@ -29,23 +29,25 @@ Route::resource('income_categories','IncomeCategoriesController');
 
 
 //frontend
-/*Route::get('home1', 'FrontendController@home1')->name('home1');
+Route::get('home1', 'FrontendController@home1')->name('home1');
+
+
+
+// Route:: get('addCustomCategory', 'FrontendController@addCustomCategory')->name('addCustomCategory');
+
+// Route:: get('addExpense', 'FrontendController@addExpense')-> name('addExpense');
+
+// Route:: get('addIncome', 'FrontendController@addIncome')-> name('addIncome');
+
+
+// Route:: get('customCategoryTable', 'FrontendController@customCategoryTable')-> name('customCategoryTable');
+
+// Route::get('expenseTable', 'FrontendController@expenseTable')->name('expenseTable');
+
+// Route::get('incomeTable', 'FrontendController@incomeTable')->name('incomeTable');
+
 
 Route::get('user','FrontendController@user')->name('user');
-
-Route:: get('addCustomCategory', 'FrontendController@addCustomCategory')->name('addCustomCategory');
-
-Route:: get('addExpense', 'FrontendController@addExpense')-> name('addExpense');
-
-Route:: get('addIncome', 'FrontendController@addIncome')-> name('addIncome');
-
-
-Route:: get('customCategoryTable', 'FrontendController@customCategoryTable')-> name('customCategoryTable');
-
-Route::get('expenseTable', 'FrontendController@expenseTable')->name('expenseTable');
-
-Route::get('incomeTable', 'FrontendController@incomeTable')->name('incomeTable');*/
-
 //addexpense
 Route::resource('expense_list','AddExpenseController');
 
@@ -58,3 +60,17 @@ Route::resource('custom_list','AddCustomController');
 //authentication
 Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
+
+//edituser
+
+Route::get('useredit', 'UserController@useredit')->name('useredit');
+
+Route::post('userupdate/{id}', 'UserController@userupdate')->name('userupdate');
+
+
+//change password
+
+Route:: get('changePassword', 'ChangePasswordController@index');
+
+Route::post('changePassword', 'ChangePasswordController@store')->name('changePassword');
+
